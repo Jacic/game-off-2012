cf9b0b8bd209fb3b02d76c3ea27ee8f2de7aa69b
Jacob White <jacobwgames@gmail.com>
2012-11-03 21:22:21 -0500

    attempted to get gravity working - unfinished

--- src/com/jacobwgames/ggo2012/Player.hx
+++ src/com/jacobwgames/ggo2012/Player.hx
@@ -16,6 +16,10 @@ class Player extends Entity
 	private var isActive:Bool;
 	private var image:Image;
 	private var speed:Int;
+	private var vy:Float;
+	private var maxVertSpeed:Int;
+	private var jumpsLeft:Int;
+	private var isOnGround:Bool;
 	
 	public function new(act:Bool) 
 	{
@@ -25,7 +29,12 @@ class Player extends Entity
 		image = new Image("gfx/clone.png");
 		graphic = image;
 		
+		setHitbox(96, 128);
+		type = "clone";
 		speed = 120;
+		vy = .1;
+		maxVertSpeed = 20;
+		jumpsLeft = 1;
 		
 		//set up controls
 		Input.define("up", [Key.W, Key.UP]);
@@ -37,7 +46,6 @@ class Player extends Entity
 	override public function update():Void
 	{
 		var plusX:Float = 0;
-		var plusY:Float = 0;
 		
 		if(isActive)
 		{
@@ -50,8 +58,62 @@ class Player extends Entity
 				plusX += speed * HXP.elapsed;
 			}
 			
-			x += plusX;
-			y += plusY;
+			if(Input.pressed("up") && collide("solid", x, y + 1) != null && jumpsLeft > 0)
+			{
+				jumpsLeft -= 1;
+				vy = -(maxVertSpeed * .75);
+			}
 		}
+		
+		var colsolid = collide("solid", x, y + vy);
+		if(plusX != 0 && colsolid == null)
+		{
+			//nothing in the way
+			x += plusX;
+		}
+		else if(plusX != 0)
+		{
+			//a solid object in the way
+			if(plusX > 0)
+			{
+				x = colsolid.x - image.scaledWidth;
+			}
+			else
+			{
+				x = colsolid.x + colsolid.width;
+			}
+			plusX = 0;
+		}
+		
+		colsolid = collide("solid", x, y + vy);
+		if(vy != 0 && colsolid == null)
+		{
+			//nothing in the way
+			y += HXP.sign(vy);
+		}
+		else if(vy != 0)
+		{
+			//a solid object int the way
+			if(vy > 0)
+			{
+				y = colsolid.y - image.scaledHeight;
+			}
+			else
+			{
+				y = colsolid.y + colsolid.height;
+			}
+			vy = 0;
+		}
+		
+		if(vy != 0)
+		{
+			vy += HXP.elapsed * 10;
+			if(vy > maxVertSpeed)
+			{
+				vy = maxVertSpeed;
+			}
+		}
+		
+		x += plusX;
 	}
 }
