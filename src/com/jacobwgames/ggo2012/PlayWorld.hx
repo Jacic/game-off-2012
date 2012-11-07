8da3b29a45cd78f7f55f77e5367dd93dc9a5f733
Jacob White <jacobwgames@gmail.com>
2012-11-07 09:37:28 -0600

    started level loading

--- src/com/jacobwgames/ggo2012/PlayWorld.hx
+++ src/com/jacobwgames/ggo2012/PlayWorld.hx
@@ -1,6 +1,7 @@
 package com.jacobwgames.ggo2012;
 
 import com.haxepunk.World;
+import haxe.xml.Fast;
 
 /**
  * ...
@@ -9,20 +10,36 @@ import com.haxepunk.World;
 
 class PlayWorld extends World
 {
-
-	public function new() 
+	private var levelWidth:Int;
+	private var levelHeight:Int;
+	private var clones:Array<Player>;
+	
+	public function new(level:Xml) 
 	{
 		super();
 		
-		add(new Player(true));
-		var solid:Solid = new Solid();
-		solid.x = 10;
-		solid.y = 500;
-		add(solid);
+		levelWidth = 2000;
+		levelWidth = 800;
+		
+		clones = [];
+		
+		var fast = new Fast(level.firstElement());
+		
+		for(c in fast.nodes.clone)
+		{
+			var clone:Player = new Player(true, Std.parseInt(c.att.x), Std.parseInt(c.att.y));
+			clones.push(clone);
+			add(clone);
+		}
+		for(s in fast.nodes.solid)
+		{
+			add(new Solid(Std.parseInt(s.att.x), Std.parseInt(s.att.y)));
+		}
 	}
 	
 	override public function update():Void
 	{
 		super.update();
+		
 	}
 }
