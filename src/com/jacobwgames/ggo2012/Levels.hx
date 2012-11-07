8b2da60c010e86ce510a5e5ec6a39f7feac53f9b
Jacob White <jacobwgames@gmail.com>
2012-11-07 09:37:51 -0600

    began a test level

---
+++ src/com/jacobwgames/ggo2012/Levels.hx
@@ -0,0 +1,11 @@
+package com.jacobwgames.ggo2012;
+
+/**
+ * ...
+ * @author Jacic
+ */
+
+class Levels 
+{
+	public static var level1:Xml = Xml.parse('<level><clone x="64" y="128" /><solid x="0" y="480" /><solid x="64" y="480" /></level>');
+}
