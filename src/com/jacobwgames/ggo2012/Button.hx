fef8819750779b37221817a5950bb2b101e6d402
Jacob White <jacobwgames@gmail.com>
2012-11-20 19:50:30 -0600

    first commit

---
+++ src/com/jacobwgames/ggo2012/Button.hx
@@ -0,0 +1,48 @@
+package com.jacobwgames.ggo2012;
+
+import com.haxepunk.Entity;
+import com.haxepunk.graphics.Spritemap;
+
+/**
+ * ...
+ * @author Jacic
+ */
+
+class Button extends Entity
+{
+	public var activated:Bool;
+	private var image:Spritemap;
+	
+	public function new(xx:Int, yy:Int) 
+	{
+		super();
+		
+		image = new Spritemap("gfx/button.png", 64, 32);
+		image.add("deactivated", [0], 0, false);
+		image.add("activated", [1], 0, false);
+		graphic = image;
+		
+		setHitbox(64, 32);
+		changeState(act);
+		
+		activated = false;
+		type = "solid";
+		mode = themode;
+		x = xx;
+		y = yy;
+	}
+	
+	override public function update()
+	{
+		if(collide("clone", x, y - 1) != null)
+		{
+			activated = true;
+			image.play("activated"):
+		}
+		else
+		{
+			activated = false;
+			image.play("deactivated");
+		}
+	}
+}
