a7e7a7bdf1429de2b0400c066adbae922f4a13c2
Jacob White <jacobwgames@gmail.com>
2012-11-30 19:25:39 -0600

    remove the debug console because the game is now finished :)

--- src/com/jacobwgames/ggo2012/Main.hx
+++ src/com/jacobwgames/ggo2012/Main.hx
@@ -30,7 +30,7 @@ class Main extends Engine
 		if (flash.system.Capabilities.isDebugger)
 	#end
 		{
-			HXP.console.enable();
+			//HXP.console.enable();
 		}
 #end
 		HXP.screen.color = kClearColor;
