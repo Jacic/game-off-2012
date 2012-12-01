1cf61b73023fc046d58191f82d280a96ba606e9c
Jacob White <jacobwgames@gmail.com>
2012-11-30 19:23:43 -0600

    created a fifth level

--- src/com/jacobwgames/ggo2012/Levels.hx
+++ src/com/jacobwgames/ggo2012/Levels.hx
@@ -419,4 +419,115 @@ class Levels
     <SolidSwitch id="145" x="928" y="896" active="true" />
     <Clone id="0" x="128" y="768" />
 </level>');
+	public static var level5:Xml = Xml.parse('<level width="1280" height="960" needed="4" bmode="0" switchstart="true">
+    <Solid id="0" x="1216" y="0" />
+    <Solid id="1" x="1152" y="0" />
+    <Solid id="2" x="1088" y="0" />
+    <Solid id="3" x="1024" y="0" />
+    <Solid id="4" x="960" y="0" />
+    <Solid id="5" x="896" y="0" />
+    <Solid id="6" x="832" y="0" />
+    <Solid id="7" x="768" y="0" />
+    <Solid id="8" x="704" y="0" />
+    <Solid id="9" x="640" y="0" />
+    <Solid id="10" x="576" y="0" />
+    <Solid id="11" x="512" y="0" />
+    <Solid id="12" x="448" y="0" />
+    <Solid id="13" x="384" y="0" />
+    <Solid id="14" x="320" y="0" />
+    <Solid id="15" x="256" y="0" />
+    <Solid id="16" x="192" y="0" />
+    <Solid id="17" x="128" y="0" />
+    <Solid id="18" x="64" y="0" />
+    <Solid id="19" x="0" y="0" />
+    <Solid id="20" x="768" y="64" />
+    <Solid id="21" x="768" y="128" />
+    <Solid id="22" x="768" y="192" />
+    <Solid id="23" x="768" y="256" />
+    <Solid id="24" x="768" y="320" />
+    <Solid id="25" x="1024" y="384" />
+    <Solid id="26" x="1024" y="448" />
+    <Solid id="27" x="1248" y="64" />
+    <Solid id="28" x="1248" y="128" />
+    <Solid id="29" x="1248" y="192" />
+    <Solid id="30" x="1248" y="256" />
+    <Solid id="31" x="1248" y="320" />
+    <Solid id="32" x="1248" y="384" />
+    <Solid id="33" x="1248" y="448" />
+    <Solid id="34" x="1248" y="512" />
+    <Solid id="35" x="1248" y="576" />
+    <Solid id="36" x="1248" y="640" />
+    <Solid id="37" x="1248" y="704" />
+    <Solid id="38" x="1248" y="768" />
+    <Solid id="39" x="1248" y="832" />
+    <Solid id="40" x="1248" y="896" />
+    <Solid id="41" x="1184" y="896" />
+    <Solid id="42" x="0" y="896" />
+    <Solid id="43" x="0" y="832" />
+    <Solid id="44" x="0" y="768" />
+    <Solid id="45" x="0" y="64" />
+    <Solid id="46" x="0" y="128" />
+    <Solid id="47" x="0" y="192" />
+    <Solid id="48" x="0" y="256" />
+    <Solid id="49" x="0" y="320" />
+    <Solid id="50" x="0" y="384" />
+    <Solid id="51" x="0" y="448" />
+    <Solid id="52" x="0" y="512" />
+    <Solid id="53" x="0" y="576" />
+    <Solid id="54" x="0" y="640" />
+    <Solid id="55" x="0" y="704" />
+    <Solid id="56" x="64" y="896" />
+    <Solid id="57" x="128" y="896" />
+    <Solid id="58" x="192" y="896" />
+    <Solid id="59" x="256" y="896" />
+    <Solid id="60" x="320" y="896" />
+    <Solid id="61" x="384" y="896" />
+    <Solid id="62" x="448" y="896" />
+    <Solid id="63" x="512" y="896" />
+    <Solid id="64" x="576" y="896" />
+    <Solid id="65" x="640" y="896" />
+    <Solid id="66" x="704" y="896" />
+    <Solid id="67" x="768" y="896" />
+    <Solid id="68" x="832" y="896" />
+    <Solid id="69" x="896" y="896" />
+    <Solid id="70" x="960" y="896" />
+    <Solid id="71" x="1024" y="896" />
+    <Solid id="72" x="1088" y="896" />
+    <Solid id="73" x="1152" y="896" />
+    <Solid id="74" x="1024" y="512" />
+    <Solid id="75" x="1024" y="576" />
+    <Solid id="76" x="1024" y="640" />
+    <Solid id="77" x="768" y="384" />
+    <Solid id="78" x="768" y="448" />
+    <Solid id="79" x="768" y="512" />
+    <Solid id="80" x="768" y="576" />
+    <Solid id="81" x="768" y="640" />
+    <SolidSwitch id="82" x="832" y="384" active="true" />
+    <SolidSwitch id="83" x="864" y="384" active="true" />
+    <SolidSwitch id="84" x="896" y="384" active="true" />
+    <SolidSwitch id="85" x="928" y="384" active="true" />
+    <SolidSwitch id="86" x="960" y="384" active="true" />
+    <SolidSwitch id="87" x="992" y="384" active="true" />
+    <Solid id="89" x="832" y="640" />
+    <Solid id="90" x="896" y="640" />
+    <Solid id="91" x="960" y="640" />
+    <Solid id="88" x="64" y="832" />
+    <Solid id="92" x="64" y="768" />
+    <Solid id="93" x="64" y="704" />
+    <Solid id="94" x="64" y="352" />
+    <Solid id="95" x="128" y="832" />
+    <Solid id="96" x="128" y="768" />
+    <Solid id="97" x="128" y="704" />
+    <Solid id="98" x="192" y="832" />
+    <Solid id="99" x="192" y="768" />
+    <Solid id="100" x="256" y="832" />
+    <Solid id="101" x="704" y="544" />
+    <Solid id="102" x="512" y="544" />
+    <Solid id="103" x="448" y="544" />
+    <Solid id="104" x="384" y="544" />
+    <Solid id="105" x="320" y="544" />
+    <Goal id="106" x="896" y="608" />
+    <Button id="107" x="704" y="512" />
+    <Clone id="0" x="864" y="256" />
+</level>');
 }
